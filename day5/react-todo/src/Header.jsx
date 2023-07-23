c4d156e29fd7e35039f2e7284c32be6eeeba1f67
Aung662001 <setannatsoegodzzz@gmail.com>
2023-07-23 21:00:25 +0630

    rotate dark/light btn

--- day5/react-todo/src/Header.jsx
+++ day5/react-todo/src/Header.jsx
@@ -4,6 +4,8 @@ import { Badge, Typography, Toolbar, Box, AppBar, Button } from "@mui/material";
 import { ThemeContext } from "./ThemeApp";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
 import LightModeIcon from "@mui/icons-material/LightMode";
+import { delay, motion } from "framer-motion";
+import { Rotate90DegreesCcw } from "@mui/icons-material";
 
 export default function ButtonAppBar({ count }) {
   const { setMode, mode } = React.useContext(ThemeContext);
@@ -22,11 +24,21 @@ export default function ButtonAppBar({ count }) {
           </Typography>
           {mode === "dark" ? (
             <Button color="inherit" onClick={changeMode}>
-              <LightModeIcon />
+              <motion.div
+                whileTap={{ rotate: [0, 90, 180], opacity: [1] }}
+                transition={{ duration: 0.3 }}
+              >
+                <LightModeIcon />
+              </motion.div>
             </Button>
           ) : (
             <Button color="inherit" onClick={changeMode}>
-              <DarkModeIcon />
+              <motion.div
+                whileTap={{ rotate: [0, 360], opacity: [1] }}
+                transition={{ duration: 0.3 }}
+              >
+                <DarkModeIcon />
+              </motion.div>
             </Button>
           )}
         </Toolbar>
